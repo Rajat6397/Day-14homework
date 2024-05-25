6e140adf045e1f5e4255844c813056f8cb9eb739
Rajat <rajatsaxena6397@gmail.com>
2024-05-25 23:55:55 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>My Budget Planner</h1>
+      <div className="second">
+      <button>Budget:Rs.2000</button>
+      <button>Remaining:Rs.2000</button>
+      <button>Spant so far:0</button>
+      </div>
+    </>
+  )
+}
+
+export default App
