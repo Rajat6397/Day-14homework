285ed3a751d17d43b374aa35e0d3f525a5e72ee9
Rajat <rajatsaxena6397@gmail.com>
2024-05-26 00:27:11 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,20 @@ function App() {
       <button>Remaining:Rs.2000</button>
       <button>Spant so far:0</button>
       </div>
+      <h1>Expenses</h1>
+      <h2>Add Data to List....</h2>
+      <h2>Add Expenses</h2>
+      <div className="entry">
+      <div className="given">
+      <h4>Name</h4>
+      <input type="text"/>
+      </div>
+      <div className="output">
+        <h4>Cost</h4>
+      <input type="text"/>
+      </div>
+      </div>
+      <button id="but">Save</button>
     </>
   )
 }
